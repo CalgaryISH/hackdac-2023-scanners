93f3081be5b110716c1be383e9c4a3b19b9d3b3b
joey <joeyah20@gmail.com>
2023-07-11 02:39:44 -0500

    added AND scanner

--- sv-param-scanner/scanner.h
+++ sv-param-scanner/scanner.h
@@ -7,6 +7,7 @@
 #include <slang/diagnostics/DiagnosticEngine.h>
 #include <slang/diagnostics/DiagnosticClient.h>
 #include <slang/diagnostics/TextDiagnosticClient.h>
+#include <slang/numeric/SVInt.h>
 
 #include <boost/algorithm/string.hpp>
 
@@ -116,22 +117,106 @@ struct CaseVisitor : public SyntaxVisitor<CaseVisitor> {
         this->treeSourceManager = sm;
     }
 
-    void handle(const CaseStatementSyntax &t) {
-        bool hasDefaultStmt = false;
-        for(auto & caseItem : t.items) {
-            if(caseItem->kind == syntax::SyntaxKind::DefaultCaseItem) {
-                hasDefaultStmt = true;
+    // void handle(const CaseStatementSyntax &t) {
+    //     bool hasDefaultStmt = false;
+    //     for(auto & caseItem : t.items) {
+    //         if(caseItem->kind == syntax::SyntaxKind::DefaultCaseItem) {
+    //             hasDefaultStmt = true;
+    //         }
+    //         // std::cout << caseItem->kind << "\n";
+
+    //     }
+
+    //     // //parent and child have matching param identifiers; child doesn't assign parameter to parent identifier
+    //     if(!hasDefaultStmt) {
+
+    //         std::string_view filename = treeSourceManager->getFileName(t.sourceRange().start());
+    //         std::size_t lineNum = treeSourceManager->getLineNumber(t.sourceRange().start());
+    //         std::cerr << "Warning - In file " << filename << ", line " << lineNum << ": " << "No default case\n";
+    //     }
+    // }
+    bool isZero;
+    void handle(const BinaryExpressionSyntax &t) {
+        if(t.kind == syntax::SyntaxKind::BinaryAndExpression) {
+            auto lhs = t.left;
+            auto rhs = t.right;
+            if(lhs && rhs) {
+                if(lhs->kind == syntax::SyntaxKind::IntegerLiteralExpression) {
+                    isZero = false;
+                    lhs->visit(*this);
+
+                    if(isZero) {
+                        std::string_view filename = treeSourceManager->getFileName(t.sourceRange().start());
+                        std::size_t lineNum = treeSourceManager->getLineNumber(t.sourceRange().start());
+                        std::cerr << "Warning - In file " << filename << ", line " << lineNum << ": " << "LHS of AND is zero\n";
+                        return;
+                    }
+                }
+                if(rhs->kind == syntax::SyntaxKind::IntegerLiteralExpression) {
+                    isZero = false;
+                    rhs->visit(*this);
+                    if(isZero) {
+                        std::string_view filename = treeSourceManager->getFileName(t.sourceRange().start());
+                        std::size_t lineNum = treeSourceManager->getLineNumber(t.sourceRange().start());
+                        std::cerr << "Warning - In file " << filename << ", line " << lineNum << ": " << "RHS of AND is zero\n";
+                        return;
+                    }
+                }
+                
             }
-            // std::cout << caseItem->kind << "\n";
-
         }
+    }
 
-        // //parent and child have matching param identifiers; child doesn't assign parameter to parent identifier
-        if(!hasDefaultStmt) {
+    void handle (const LiteralExpressionSyntax &t) {
+        if(t.kind == syntax::SyntaxKind::IntegerLiteralExpression) {
+            isZero = exactlyEqual(t.literal.intValue(), SVInt(0));
+        }
+    }
+    
+};
 
-            std::string_view filename = treeSourceManager->getFileName(t.sourceRange().start());
-            std::size_t lineNum = treeSourceManager->getLineNumber(t.sourceRange().start());
-            std::cerr << "Warning - In file " << filename << ", line " << lineNum << ": " << "No default case\n";
+
+struct ANDVisitor : public SyntaxVisitor<ANDVisitor> {
+    SourceManager *treeSourceManager;
+    ANDVisitor(SourceManager *sm) {
+        this->treeSourceManager = sm;
+    }
+
+    bool isZero;
+    void handle(const BinaryExpressionSyntax &t) {
+        if(t.kind == syntax::SyntaxKind::BinaryAndExpression) {
+            auto lhs = t.left;
+            auto rhs = t.right;
+            if(lhs && rhs) {
+                if(lhs->kind == syntax::SyntaxKind::IntegerLiteralExpression) {
+                    isZero = false;
+                    lhs->visit(*this);
+
+                    if(isZero) {
+                        std::string_view filename = treeSourceManager->getFileName(t.sourceRange().start());
+                        std::size_t lineNum = treeSourceManager->getLineNumber(t.sourceRange().start());
+                        std::cerr << "Warning - In file " << filename << ", line " << lineNum << ": " << "LHS of AND is zero\n";
+                        return;
+                    }
+                }
+                if(rhs->kind == syntax::SyntaxKind::IntegerLiteralExpression) {
+                    isZero = false;
+                    rhs->visit(*this);
+                    if(isZero) {
+                        std::string_view filename = treeSourceManager->getFileName(t.sourceRange().start());
+                        std::size_t lineNum = treeSourceManager->getLineNumber(t.sourceRange().start());
+                        std::cerr << "Warning - In file " << filename << ", line " << lineNum << ": " << "RHS of AND is zero\n";
+                        return;
+                    }
+                }
+                
+            }
+        }
+    }
+
+    void handle (const LiteralExpressionSyntax &t) {
+        if(t.kind == syntax::SyntaxKind::IntegerLiteralExpression) {
+            isZero = exactlyEqual(t.literal.intValue(), SVInt(0));
         }
     }
 };
@@ -144,12 +229,14 @@ private:
     //Parameter visitor
     ParameterVisitor *paramVisitor;
     CaseVisitor *caseVisitor;
+    ANDVisitor * andVisitor;
 
 public:
     ParameterScanner(std::shared_ptr<SyntaxTree> tree) {
         this->root = &(tree->root());
         this->paramVisitor = new ParameterVisitor(&(tree->sourceManager()));
         this->caseVisitor = new CaseVisitor(&(tree->sourceManager()));
+        this->andVisitor = new ANDVisitor(&(tree->sourceManager()));
 
     }
     
@@ -163,7 +250,8 @@ public:
     }
 
     void scan() {
-        root->visit(*paramVisitor);
-        root->visit(*caseVisitor);
+        // root->visit(*paramVisitor);
+        // root->visit(*caseVisitor);
+        root->visit(*andVisitor);
     }
 };
